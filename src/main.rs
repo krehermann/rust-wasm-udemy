962be9e79253af7a72f9b681347516f6ad05f07c
Keith Rehermann <krehermann@nvidia.com>
2022-10-23 15:53:29 -0600

    finish move and copy, include some drawings

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,6 @@ fn get_welcome() -> &'static str{
 }
 
 
-
 fn primitive_aside() {
     let custom_num = 98_000;
     let hex_num = 0xfa;
@@ -24,4 +23,50 @@ fn primitive_aside() {
     println!("{}", hex_num);
     println!("{}", bin_num);
     println!("{}", byte_num);
+}
+
+pub fn string_memory() -> String {
+   let temp = String::from("a string put a ptr, capacity, and lenght on the stack. the value is on the heap");
+   // moving non-primitive type causes the a new stack frame to be allocated for the 
+   // content. the old stack content, in this case temp, is no longer valid and cannot be addressed
+   // Rust's mv'ing semantics prevents double freeing. That is, allowing cp'ing of ptrs on the stack, which is the representation of
+   // of a complex type, leads to multiple stack frames referencing the same heap location.
+   // Mv'ing have strong ownership properties that map from the stack to the heap, makind the double free problem go away.
+   let output = temp;
+   output
+}
+
+
+fn extend_string(mut s: String) -> String {
+    s.push_str(" and more...");
+    s
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_welcome_works() {
+        let result = get_welcome();
+        assert_eq!(result, "hi");
+    }
+
+    #[test]
+    fn return_mved_value() {
+        let msg= String::from("Hello");
+        // we can get the value of an immutable var 
+        // 1. by shadowing
+        let msg = extend_string(msg);
+        assert_eq!(msg, "Hello and more...");
+        // 2. with a new variable
+        let orig = String::from("Hello");
+        let updated = extend_string(orig);
+        assert_eq!(updated, "Hello and more...");
+        // or by using mutables in the first place
+        let mut mutant  = String::from("Hello");
+        mutant = extend_string(mutant);
+        assert_eq!(mutant, "Hello and more...");
+
+    }
 }
