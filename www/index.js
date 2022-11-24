6626fd032c8b497b1455c255306b611ec84e8520
Keith Rehermann <krehermann@nvidia.com>
2022-11-23 18:39:09 -0700

    importing javascript into wasm
    to reproduce: copy
    
    (module
        (import "myconsole" "log" (func $log))
        (func $sum (param $a i32) (param $b i32) (result i32)
            call $log
            local.get $a
            local.get $b
            i32.add
        )
        (export "sum"( func $sum))
    )
    to
    https://webassembly.github.io/wabt/demo/wat2wasm/index.html

--- www/index.js
+++ www/index.js
@@ -1,9 +1,20 @@
 async function init() {
-     
+    
+    const importObject = {
+        myconsole: {
+            log: () => {
+                console.log("logging...")
+            },
+            error: () => {
+                console.log("error...")
+            }
+        }
+    }
     // load the wasm file
-    const resp = await fetch("sum.wasm");
+    const resp = await fetch("sum_with_logging.wasm");
     const intructions = await resp.arrayBuffer()
-    const wasm = await WebAssembly.instantiate(intructions);
+    const wasm = await WebAssembly.instantiate(intructions, importObject);
+    debugger
     const sumFn = wasm.instance.exports.sum;
     const result = sumFn(250, 300);
     console.log(result);
